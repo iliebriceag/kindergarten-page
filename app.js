ea0bcb1012217079d46baf4b8ad211f04895d8df
iliebriceag <74901370+iliebriceag@users.noreply.github.com>
2023-07-06 17:53:53 +0300

    kinder

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ $(window).scroll(function () {
     $(".colorNavbar").toggleClass('fixed', $(window).scrollTop() > 50);
 });
 
-// Active Butoon
+// Active Button
 $(document).ready(function () {
     $("ul > li a").click(function () {
             $('ul > li a').removeClass('active');
